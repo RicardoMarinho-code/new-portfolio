0df038270ead2e2792608fa60ecc55c504043428
Ricardo Marinho <Ricardomarinho.vaga@gmail.com>
2026-03-25 13:31:23 -0300

    .

---
+++ app/layouts/navbar/nav-data.js
@@ -0,0 +1,43 @@
+import config from '~/config.json';
+
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Github',
+    url: `https://github.com/${config.github}`,
+    icon: 'github'
+  },
+  {
+    label: 'Linkedin',
+    url: `https://www.linkedin.com/in/${config.linkedin}`,
+    icon: 'linkedin'
+  },
+  {
+    label: 'Workana',
+    url: `https://www.workana.com/freelancer/${config.workana}`,
+    icon: 'workana'
+  },
+  {
+    label: 'Instagram',
+    url: `https://www.instagram.com/${config.instagram}`,
+    icon: 'instagram'
+  }
+];
